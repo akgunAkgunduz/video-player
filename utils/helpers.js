7ec804d2dd7f9b71b76d48549964b3de59eadc1a
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-09-25 15:05:40 +0300

    Add helper functions

---
+++ utils/helpers.js
@@ -0,0 +1,9 @@
+exports.generateTimeText = (seconds) => {
+  const string = new Date(seconds * 1000).toISOString()
+  return seconds < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+}
+
+exports.generatePositionText = (seconds, duration) => {
+  const string = new Date(seconds * 1000).toISOString()
+  return duration < 3600 ? string.substr(14, 5) : string.substr(11, 8)
+}

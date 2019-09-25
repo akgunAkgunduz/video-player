02c6b7bc5ecd0d654a099e62b90f899d805bd00c
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-09-25 16:33:15 +0300

    Add UI elements module

---
+++ utils/ui-elements.js
@@ -0,0 +1,12 @@
+module.exports = {
+  videoContainer: document.getElementById('video-container'),
+  video: document.getElementById('video'),
+  progressBar: document.getElementById('progress-bar'),
+  progressBarInput: document.getElementById('progress-bar-input'),
+  progressBarThumb: document.getElementById('progress-bar-thumb'),
+  progressBarProgress: document.getElementById('progress-bar-progress'),
+  progressBarBackground: document.getElementById('progress-bar-background'),
+  progressBarInfo: document.getElementById('progress-bar-info'),
+  timeInfo: document.getElementById('time-info'),
+  playPauseToggle: document.getElementById('play-pause-toggle'),
+}

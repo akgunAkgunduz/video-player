0baf8b87f2d2bb3dce18bdeb26e688848eca2e5e
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-09-26 10:31:31 +0300

    Add Controller class

---
+++ classes/controller.js
@@ -0,0 +1,45 @@
+const Player = require('./player')
+const View = require('./view')
+const uiElements = require('../utils/ui-elements')
+
+const player = new Player(document.getElementById('video'))
+const view = new View(uiElements)
+
+class Controller {
+  setUpEventListeners() {
+    player.media.addEventListener('loadedmetadata', () => {
+      view.elements.progressBarInput.max = player.media.duration
+
+      view.resizeVideo()
+    })
+
+    player.media.addEventListener('timeupdate', () => {
+      view.updateTimeInfo(player)
+      view.updateProgressBar(player)
+    })
+
+    player.media.addEventListener('ended', () => {
+      view.updatePlayPauseToggle(player)
+    })
+
+    view.elements.progressBarInput.addEventListener('input', (e) => {
+      player.media.currentTime = e.target.value
+
+      view.updateProgressBar(player)
+    })
+
+    view.elements.progressBarInput.addEventListener('mousemove', (event) => {
+      view.showProgressBarInfo(player, event)
+    })
+
+    view.elements.playPauseToggle.addEventListener('click', () => {
+      player.toggle()
+
+      view.updatePlayPauseToggle(player)
+    })
+
+    window.addEventListener('resize', view.resizeVideo)
+  }
+}
+
+module.exports = Controller

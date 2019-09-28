22775285a7159516df833e1ca7c7a87e48c33595
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-09-28 21:51:18 +0300

    Update title bar

--- classes/titleBar.js
+++ classes/titleBar.js
@@ -17,11 +17,39 @@ class TitleBar {
       this.elements.maximize.querySelector('i').innerHTML = 'unfold_more'
     })
 
+    win.on('minimize', () => {
+      this.elements.minimize.style.backgroundColor = '#333'
+    })
+
     this.elements.minimize.addEventListener('click', () => win.minimize())
 
     this.elements.maximize.addEventListener('click', () => win.isMaximized() ? win.unmaximize() : win.maximize())
 
     this.elements.close.addEventListener('click', () => win.close())
+
+    this.elements.minimize.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = '#555'
+    })
+
+    this.elements.minimize.addEventListener('mouseleave', (e) => {
+      e.target.style.backgroundColor = '#333'
+    })
+
+    this.elements.maximize.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = '#555'
+    })
+
+    this.elements.maximize.addEventListener('mouseleave', (e) => {
+      e.target.style.backgroundColor = '#333'
+    })
+
+    this.elements.close.addEventListener('mouseenter', (e) => {
+      e.target.style.backgroundColor = '#f00'
+    })
+
+    this.elements.close.addEventListener('mouseleave', (e) => {
+      e.target.style.backgroundColor = '#333'
+    })
   }
 }
 

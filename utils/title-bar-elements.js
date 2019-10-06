f865c7aef7d5744adcb1e3b03f7a59859ba0c48c
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-10-06 19:15:21 +0300

    Update title bar elements module

--- utils/title-bar-elements.js
+++ utils/title-bar-elements.js
@@ -1,4 +1,7 @@
 module.exports = {
+  bar: document.getElementById('title-bar'),
+  icon: document.getElementById('app-icon'),
+  title: document.getElementById('app-name'),
   minimize: document.getElementById('minimize'),
   maximize: document.getElementById('maximize'),
   close: document.getElementById('close'),

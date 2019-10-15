16e2cebba81ec1839fbbd43358645bd99759f3e4
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-10-15 21:01:40 +0300

    Update helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -7,3 +7,5 @@ exports.generatePositionText = (seconds, duration) => {
   const string = new Date(seconds * 1000).toISOString()
   return duration < 3600 ? string.substr(14, 5) : string.substr(11, 8)
 }
+
+exports.sanitizeFilePath = path => path.replace('#', '%23')

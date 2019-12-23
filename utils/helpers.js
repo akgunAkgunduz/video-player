b34cdb158dfaa1ad8bbbe8bf30646599f7dfd45f
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-12-23 16:32:39 +0300

    Update helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,19 +1,11 @@
-exports.generateTimeText = (seconds) => {
-  const string = new Date(seconds * 1000).toISOString()
+const supportedFileTypes = require('./supported-file-types')
 
-  return seconds < 3600 ? string.substr(14, 5) : string.substr(11, 8)
-}
+exports.isFileTypeSupported = filePath => supportedFileTypes.includes(filePath.split('.').pop().toLowerCase())
 
-exports.generatePositionText = (seconds, duration) => {
-  const string = new Date(seconds * 1000).toISOString()
-  
-  return duration < 3600 ? string.substr(14, 5) : string.substr(11, 8)
-}
+exports.sanitizeFilePath = path => path.replace('#', '%23')
 
 exports.formatSeconds = (position, duration) => {
   const string = new Date(position * 1000).toISOString()
 
   return (duration || position) < 3600 ? string.substr(14, 5) : string.substr(11, 8) 
 }
-
-exports.sanitizeFilePath = path => path.replace('#', '%23')

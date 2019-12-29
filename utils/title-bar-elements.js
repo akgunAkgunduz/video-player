88fafb5013185d9d211acb6523684a79325cae86
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2019-12-29 19:51:39 +0300

    Add keyboard shortcuts button to title bar

--- utils/title-bar-elements.js
+++ utils/title-bar-elements.js
@@ -2,6 +2,7 @@ module.exports = {
   bar: document.getElementById('title-bar'),
   icon: document.getElementById('app-icon'),
   title: document.getElementById('app-name'),
+  keyboardShortcutsButton: document.getElementById('keyboard-shortcuts-button'),
   minimize: document.getElementById('minimize'),
   maximize: document.getElementById('maximize'),
   close: document.getElementById('close'),

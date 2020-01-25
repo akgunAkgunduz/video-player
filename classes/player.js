a384f03641585ecb17d0cf2922d544a37932f3e0
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2020-01-25 12:01:27 +0300

    Add "on" method for Player

--- classes/player.js
+++ classes/player.js
@@ -32,6 +32,40 @@ class Player {
   toggle() {
     this.isPaused ? this.play() : this.pause()
   }
+
+  on(eventNames, handler) {
+    let events = []
+    Array.isArray(eventNames) ? events = [ ...eventNames ] : events.push(eventNames)
+
+    events.forEach(event => {
+      switch (event) {
+        case 'error':
+          this.media.addEventListener('error', handler)
+          break
+        case 'load':
+          this.media.addEventListener('loadedmetadata', handler)
+          break
+        case 'play':
+          this.media.addEventListener('play', handler)
+          break
+        case 'pause':
+          this.media.addEventListener('pause', handler)
+          break
+        case 'end':
+          this.media.addEventListener('ended', handler)
+          break
+        case 'playback-position-change':
+          this.media.addEventListener('timeupdate', handler)
+          break
+        case 'volume-change':
+          this.media.addEventListener('volumechange', handler)
+          break
+        case 'speed-change':
+          this.media.addEventListener('ratechange', handler)
+          break
+      }
+    })
+  }
 }
 
 module.exports = Player

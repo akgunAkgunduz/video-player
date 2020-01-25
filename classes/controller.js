089a6c2d6d8b07b580e76338b4d91d88afce5861
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2020-01-25 10:30:00 +0300

    Add keyboard events module

--- classes/controller.js
+++ classes/controller.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { ipcRenderer, remote } = require('electron')
 const Player = require('./player')
 const View = require('./view')
+const { setUpKeyboardShortcuts } = require('../events/keyboard')
 const uiElements = require('../utils/ui-elements')
 const { sanitizeFilePath, isFileTypeSupported } = require('../utils/helpers')
 
@@ -193,113 +194,11 @@ class Controller {
     })
   }
 
-  setUpEventListenersForKeyboard() {
-    document.addEventListener('keydown', (event) => {
-      if (event.code === 'Home' && !view.elements.progressBarInput.disabled) {
-        player.media.currentTime = 0
-
-        view.showMessage('position')
-      }
-
-      if (event.code === 'ArrowLeft' && !event.ctrlKey && !view.elements.progressBarInput.disabled) {
-        player.media.currentTime -= 5
-
-        view.showMessage('position')
-      }
-
-      if (event.code === 'ArrowRight' && !event.ctrlKey && !view.elements.progressBarInput.disabled) {
-        player.media.currentTime += 5
-
-        view.showMessage('position')
-      }
-
-      if (event.code === 'ArrowLeft' && event.ctrlKey && !view.elements.progressBarInput.disabled) {
-        player.media.currentTime -= 15
-
-        view.showMessage('position')
-      }
-
-      if (event.code === 'ArrowRight' && event.ctrlKey && !view.elements.progressBarInput.disabled) {
-        player.media.currentTime += 15
-
-        view.showMessage('position')
-      }
-      
-      if (event.code === 'KeyO' && event.ctrlKey) {
-        view.elements.openFileButton.click()
-      }
-      
-      if (event.code === 'Space') {
-        view.elements.playPauseToggle.click()
-        
-        view.showMessage('status')
-      }
-      
-      if (event.code === 'KeyM') {
-        view.elements.muteButton.click()
-        
-        view.showMessage('volume')
-      }
-      
-      if (event.code === 'ArrowUp' && !event.ctrlKey) {
-        view.elements.volumeSlider.value = parseFloat(view.elements.volumeSlider.value) + 0.05
-        view.elements.volumeSlider.dispatchEvent(new Event('input'))
-
-        view.showMessage('volume')
-      }
-
-      if (event.code === 'ArrowDown' && !event.ctrlKey) {
-        view.elements.volumeSlider.value = parseFloat(view.elements.volumeSlider.value) - 0.05
-        view.elements.volumeSlider.dispatchEvent(new Event('input'))
-
-        view.showMessage('volume')
-      }
-
-      if (event.code === 'KeyS') {
-        view.elements.resetSpeedButton.click()
-
-        view.showMessage('speed')
-      }
-
-      if (event.code === 'ArrowUp' && event.ctrlKey) {
-        view.elements.speedSlider.value = parseFloat(view.elements.speedSlider.value) + 0.05
-        view.elements.speedSlider.dispatchEvent(new Event('input'))
-
-        view.showMessage('speed')
-      }
-
-      if (event.code === 'ArrowDown' && event.ctrlKey) {
-        view.elements.speedSlider.value = parseFloat(view.elements.speedSlider.value) - 0.05
-        view.elements.speedSlider.dispatchEvent(new Event('input'))
-
-        view.showMessage('speed')
-      }
-
-      if (event.code === 'KeyR') {
-        view.elements.repeatToggle.click()
-
-        view.showMessage('repeat')
-      }
-
-      if (event.code === 'F11' && view.elements.fullscreenButton.disabled) {
-        event.preventDefault()
-      }
-
-      if (event.code === 'KeyF') {
-        view.elements.fullscreenButton.click()
-      }
-
-      if (event.code === 'Escape' && win.isFullScreen()) {
-        view.elements.fullscreenButton.click()
-      }
-    })
-  }
-
   setUpEventListeners() {
     this.setUpEventListenersForPlayer()
     this.setUpEventListenersForView()
     this.setUpEventListenersForWindow()
-    this.setUpEventListenersForKeyboard()
+    setUpKeyboardShortcuts(player, view)
 
     ipcRenderer.on('selected-file', (event, filePath) => {
       if (filePath) {
---
+++ events/keyboard.js
@@ -0,0 +1,104 @@
+const { remote } = require('electron')
+const win = remote.getCurrentWindow()
+
+exports.setUpKeyboardShortcuts = (player, view) => {
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Home' && !view.elements.progressBarInput.disabled) {
+      player.media.currentTime = 0
+
+      view.showMessage('position')
+    }
+
+    if (event.code === 'ArrowLeft' && !event.ctrlKey && !view.elements.progressBarInput.disabled) {
+      player.media.currentTime -= 5
+
+      view.showMessage('position')
+    }
+
+    if (event.code === 'ArrowRight' && !event.ctrlKey && !view.elements.progressBarInput.disabled) {
+      player.media.currentTime += 5
+
+      view.showMessage('position')
+    }
+
+    if (event.code === 'ArrowLeft' && event.ctrlKey && !view.elements.progressBarInput.disabled) {
+      player.media.currentTime -= 15
+
+      view.showMessage('position')
+    }
+
+    if (event.code === 'ArrowRight' && event.ctrlKey && !view.elements.progressBarInput.disabled) {
+      player.media.currentTime += 15
+
+      view.showMessage('position')
+    }
+    
+    if (event.code === 'KeyO' && event.ctrlKey) {
+      view.elements.openFileButton.click()
+    }
+    
+    if (event.code === 'Space') {
+      view.elements.playPauseToggle.click()
+      
+      view.showMessage('status')
+    }
+    
+    if (event.code === 'KeyM') {
+      view.elements.muteButton.click()
+      
+      view.showMessage('volume')
+    }
+    
+    if (event.code === 'ArrowUp' && !event.ctrlKey) {
+      view.elements.volumeSlider.value = parseFloat(view.elements.volumeSlider.value) + 0.05
+      view.elements.volumeSlider.dispatchEvent(new Event('input'))
+
+      view.showMessage('volume')
+    }
+
+    if (event.code === 'ArrowDown' && !event.ctrlKey) {
+      view.elements.volumeSlider.value = parseFloat(view.elements.volumeSlider.value) - 0.05
+      view.elements.volumeSlider.dispatchEvent(new Event('input'))
+
+      view.showMessage('volume')
+    }
+
+    if (event.code === 'KeyS') {
+      view.elements.resetSpeedButton.click()
+
+      view.showMessage('speed')
+    }
+
+    if (event.code === 'ArrowUp' && event.ctrlKey) {
+      view.elements.speedSlider.value = parseFloat(view.elements.speedSlider.value) + 0.05
+      view.elements.speedSlider.dispatchEvent(new Event('input'))
+
+      view.showMessage('speed')
+    }
+
+    if (event.code === 'ArrowDown' && event.ctrlKey) {
+      view.elements.speedSlider.value = parseFloat(view.elements.speedSlider.value) - 0.05
+      view.elements.speedSlider.dispatchEvent(new Event('input'))
+
+      view.showMessage('speed')
+    }
+
+    if (event.code === 'KeyR') {
+      view.elements.repeatToggle.click()
+
+      view.showMessage('repeat')
+    }
+
+    if (event.code === 'F11' && view.elements.fullscreenButton.disabled) {
+      event.preventDefault()
+    }
+
+    if (event.code === 'KeyF') {
+      view.elements.fullscreenButton.click()
+    }
+
+    if (event.code === 'Escape' && win.isFullScreen()) {
+      view.elements.fullscreenButton.click()
+    }
+  })
+}
